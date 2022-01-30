f2d28cda75d76cea35741d9fde7759b614f98492
JRotko <76393423+JRotko@users.noreply.github.com>
2022-01-30 19:28:57 +0200

    valmis

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+
+  const add = () => {
+    setData([...data, { key: text }]);
+    setText('');
+  }
+
+  const clear = () => {
+    setData([]);
+    
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.top}>
+        <TextInput style={styles.input} onChangeText={text => setText(text)} value={text} />
+      </View>
+      <View style={styles.mid}>
+        <Button onPress={add} title="Add" />
+        <Button onPress={clear} title="Clear" />
+      </View>
+      <View style={styles.bot}>
+        <FlatList
+          data={data}
+          renderItem={({ item }) =>
+            <Text>{item.key}</Text>
+          }
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +44,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    
+  },
+  input: {
+    marginTop: 50,
+    marginBottom: 5,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1 
+  },
+  top: {
+    flex: 8,
+    justifyContent: 'flex-end'
+  },
+  mid: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  bot: {
+    flex: 10,
+    justifyContent: 'flex-start'
   },
 });
